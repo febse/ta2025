b324eee7c96322024ba2458b01e9fa29a5a31c06
Boyko Amarov <boyko.amarov@gmail.com>
2025-11-30 16:10:50 +0200

    Update

--- video_scripts/vector_movement.py
+++ video_scripts/vector_movement.py
@@ -15,9 +15,7 @@ import numpy as np
 
 
 class VectorDefinition(Scene):
-    def construct(self):
-        # Set up the coordinate system
-
+    def create_grid_and_axes(self):
         x_axis_start = -3
         x_axis_end = 4
 
@@ -46,7 +44,7 @@ class VectorDefinition(Scene):
             }
         )
 
-        # Group grid and axes for perfect alignment
+                # Group grid and axes for perfect alignment
         axes_group = VGroup(grid, axes).next_to(RIGHT, buff=0.5)
 
         # Add axis labels
@@ -55,10 +53,7 @@ class VectorDefinition(Scene):
             y_label=Text("EUR", font_size=20)
         )
 
-        # Title
-        title = Text("Bank accounts in a plane", font_size=22).to_corner(UL)
-
-        # Manual tick labels (avoid add_numbers argument limit in current Manim version)
+       # Manual tick labels (avoid add_numbers argument limit in current Manim version)
         x_tick_values = [xv for xv in range(x_axis_start, x_axis_end) if xv != 0]
         y_tick_values = [yv for yv in range(y_axis_start, y_axis_end) if yv != 0]
 
@@ -71,7 +66,17 @@ class VectorDefinition(Scene):
         y_labels = VGroup()
         for yv in y_tick_values:
             lbl = Text(str(yv), font_size=18).next_to(axes.c2p(-0.06, yv), LEFT, buff=0.05)
-            y_labels.add(lbl)
+            y_labels.add(lbl)        
+
+        return grid, axes, axes_group, axes_labels, x_labels, y_labels
+
+    def construct(self):
+        # Set up the coordinate system
+
+        grid, axes, axes_group, axes_labels, x_labels, y_labels = self.create_grid_and_axes()
+
+        # Title
+        title = Text("Bank accounts in a plane", font_size=22).to_corner(UL)
 
         self.play(
             Create(axes_group),
@@ -154,86 +159,77 @@ class VectorDefinition(Scene):
 
         self.play(FadeIn(vector_text_noname), FadeIn(vector_text))
 
-        # # Starting point 1: [0, 0]
-        # start_point1 = np.array([0, 0, 0])
-        # end_point1 = start_point1 + vector_coords
+
+class VectorScaling(VectorDefinition):
+    def construct(self):
+        # Set up the coordinate system
+
+        grid, axes, axes_group, axes_labels, x_labels, y_labels = self.create_grid_and_axes()
+
+        # Title
+        title = Text("Scaling a vector", font_size=22).to_corner(UL)
+
+        self.play(
+            Create(axes_group),
+            Write(axes_labels),
+            *[FadeIn(m) for m in x_labels],
+            *[FadeIn(m) for m in y_labels],
+            Write(title)
+        )
+        self.wait()
+
+        # Define the vector [1, 2]
+        v = np.array([1, 2, 0])
+               
+        # Show the vector notation
         
-        # # Create dots for starting points
-        # dot1_start = Dot(axes.c2p(*start_point1[:2]), color=RED, radius=0.08)
+        vector_text = MathTex(r"\vec{v} = \begin{bmatrix} 1 \\ 2 \end{bmatrix}").to_corner(UR)
+
+        self.play(Write(vector_text))
+        self.wait()
+
+        self.play(FadeOut(title))
+
+        # Show the vector from the origin to (1, 2)
+
+        start_point = np.array([0, 0, 0])
+        end_point = start_point + v
+
+        vector = Arrow(
+            axes.c2p(*start_point[:2]),
+            axes.c2p(*end_point[:2]),
+            buff=0,
+            color=YELLOW,
+            stroke_width=3,
+            tip_length=0.25
+        )
+
+        self.play(GrowArrow(vector))
+        self.wait()
+
+        instr_scale_vector = Text("Scale the vector by 3", font_size=24).to_corner(UL, buff=0.7)
+
+        self.play(Write(instr_scale_vector))
+
+        # Show the scaled vector from the origin to (3, 6)
+        scaled_v = 3 * v
+        start_point2 = np.array([0, 0, 0])
+        end_point2 = start_point2 + scaled_v
         
-        # # Labels for starting points
-        # label1_start = MathTex(r"\begin{bmatrix} 0 \\ 0 \end{bmatrix}", color=RED, font_size=28).next_to(dot1_start, DOWN+RIGHT, buff=0.15)
+        vector2 = Arrow(
+            axes.c2p(*start_point2[:2]),
+            axes.c2p(*end_point2[:2]),
+            buff=0,
+            color=GREEN,
+            stroke_width=3,
+            tip_length=0.25
+        )
+
+        self.play(GrowArrow(vector2))
+        self.wait()
+
+        scaled_vector_text = MathTex(r"3 \vec{v} = \begin{bmatrix} 3 \\ 6 \end{bmatrix}").next_to(instr_scale_vector, DOWN, buff=0.5)   
+
+        self.play(Write(scaled_vector_text))
+        self.wait()
         
-        # # Show starting points
-        # self.play(
-        #     FadeIn(dot1_start),
-        #     Write(label1_start)
-        # )
-        # self.wait()
-        
-        # # # Create instruction text
-        # # instruction = Text("Apply vector [1, 2] to both points", font_size=24).to_edge(DOWN, buff=0.7)
-        # # self.play(Write(instruction))
-        # # self.wait()
-        
-        # # # Create vectors from starting points
-        # # vector1 = Arrow(
-        # #     axes.c2p(*start_point1[:2]),
-        # #     axes.c2p(*end_point1[:2]),
-        # #     buff=0,
-        # #     color=RED,
-        # #     stroke_width=3,
-        # #     max_tip_length_to_length_ratio=0.12
-        # # )
-        
-        # # # Vector labels
-        # # vector1_label = MathTex(r"\vec{v}", color=RED, font_size=28).next_to(vector1, RIGHT, buff=0.1)
-        # # vector2_label = MathTex(r"\vec{v}", color=GREEN, font_size=28).next_to(vector2, LEFT, buff=0.1)
-        
-        # # # Animate vectors
-        # # self.play(
-        # #     GrowArrow(vector1),
-        # #     Write(vector1_label),
-        # # )
-        # # self.wait()
-        
-        # # # Create dots for end points
-        # # dot1_end = Dot(axes.c2p(*end_point1[:2]), color=RED, radius=0.08)
-        # # # Labels for end points
-        # # label1_end = MathTex(r"\begin{bmatrix} 1 \\ 3 \end{bmatrix}", color=RED, font_size=28).next_to(dot1_end, UP+RIGHT, buff=0.15)
-        
-        # # # Animate movement to end points
-        # # self.play(
-        # #     FadeOut(instruction),
-        # #     FadeIn(dot1_end),
-        # #     Write(label1_end)
-        # # )
-        # # self.wait()
-        
-        # # # Show the calculation
-        # # calc_text = Text("Same vector, same displacement!", font_size=28, color=YELLOW).to_edge(DOWN, buff=0.7)
-        # # self.play(Write(calc_text))
-        # # self.wait()
-        
-        # # # (Removed stroke highlight enlargement to keep consistent arrow sizes)
-        # # self.wait()
-        
-        # # # Show formulas
-        # # formula1 = MathTex(
-        # #     r"\begin{bmatrix} 0 \\ 0 \end{bmatrix} + \begin{bmatrix} 1 \\ 2 \end{bmatrix} = \begin{bmatrix} 1 \\ 2 \end{bmatrix}",
-        # #     color=RED,
-        # #     font_size=24
-        # # ).to_corner(UR, buff=0.5)
-        
-        # # formula2 = MathTex(
-        # #     r"\begin{bmatrix} -2 \\ -1 \end{bmatrix} + \begin{bmatrix} 1 \\ 2 \end{bmatrix} = \begin{bmatrix} -1 \\ 1 \end{bmatrix}",
-        # #     color=GREEN,
-        # #     font_size=24
-        # # ).next_to(formula1, DOWN, aligned_edge=LEFT, buff=0.3)
-        
-        # # self.play(
-        # #     FadeOut(calc_text),
-        # #     Write(formula1),
-        # #     Write(formula2)
-        # # )
-        # # self.wait(2)
